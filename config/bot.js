cda94a8bf8cadd316a4e754aba8ad3797a3c2e6b
Joao Pedro Poloni Ponce <joaopedropp@ibm.com>
2019-06-19 18:22:38 -0300

    working

--- config/bot.js
+++ config/bot.js
@@ -1,17 +1,24 @@
-const watson = require('ibm-watson');
+const watson = require('watson-developer-cloud');
 const cfenv = require('cfenv');
 const fs = require('fs');
+const request = require('request');
+
+
+
 
 let vcapLocal = null,
     appEnv = null,
     appEnvOpts = {},
     conversationWorkspace, conversation;
 
+
+
+
 const initializeAppEnv = (callback) => {
     console.log('apEnvOpts:    '+JSON.stringify(appEnvOpts));
     appEnv = cfenv.getAppEnv(appEnvOpts);
     appEnv.services = appEnvOpts.vcap.services;
-
+   
     // if (appEnv.isLocal) {
     console.log('Local Conversation initialization...');
     if (appEnv.services.conversation) {
@@ -34,77 +41,78 @@ const initializeAppEnv = (callback) => {
 
 
 const initConversation = (callback) => {
+    return true
 
-    let conversationCredentials = appEnv.getServiceCreds('Conversation');
-    const conversationUsername =  conversationCredentials.username;
-    const conversationPassword =  conversationCredentials.password;
-    const conversationURL =  conversationCredentials.url;
-    const workspaceID =  conversationCredentials.workspace_id;
+    // let conversationCredentials = appEnv.getServiceCreds('Conversation');
+    // const conversationUsername =  conversationCredentials.username;
+    // const conversationPassword =  conversationCredentials.password;
+    // const conversationURL =  conversationCredentials.url;
+    // const workspaceID =  conversationCredentials.workspace_id;
 
-    conversation = watson.conversation({
-        url: conversationURL,
-        username: conversationUsername,
-        password: conversationPassword,
-        version_date: '2017-05-26',
-        version: 'v1'
-    });
+    // conversation = watson.conversation({
+    //     url: conversationURL,
+    //     username: conversationUsername,
+    //     password: conversationPassword,
+    //     version_date: '2017-05-26',
+    //     version: 'v1'
+    // });
 
-    // Check if the workspace ID is specified in the environment
-    conversationWorkspace = process.env.CONVERSATION_WORKSPACE;
+    // // Check if the workspace ID is specified in the environment
+    // conversationWorkspace = process.env.CONVERSATION_WORKSPACE;
 
-    if (!conversationWorkspace) {
+    // if (!conversationWorkspace) {
 
-        console.log('No conversation workspace configured in the environment.');;
-        console.log(`Looking for a workspace id : ${workspaceID}`);
+    //     console.log('No conversation workspace configured in the environment.');;
+    //     console.log(`Looking for a workspace id : ${workspaceID}`);
 
-        conversation.listWorkspaces((err, result) => {
-            if (err) {
-                console.log('Failed to query workspaces. Conversation will not work. ', err);
-                callback({
-                    error: true,
-                    description: "Failed to query workspaces. Conversation woll not work."
-                });
-            } else {
-                const workspace = result.workspaces.find(workspace => workspace.workspace_id === workspaceID);
-                if (workspace) {
-                    conversationWorkspace = workspace.workspace_id;
-                    console.log(`Using Watson Conversation with username ${conversationUsername}  and workspace ${conversationWorkspace}`);
-                    callback({
-                        error: false
-                    });
+    //     conversation.listWorkspaces((err, result) => {
+    //         if (err) {
+    //             console.log('Failed to query workspaces. Conversation will not work. ', err);
+    //             callback({
+    //                 error: true,
+    //                 description: "Failed to query workspaces. Conversation woll not work."
+    //             });
+    //         } else {
+    //             const workspace = result.workspaces.find(workspace => workspace.workspace_id === workspaceID);
+    //             if (workspace) {
+    //                 conversationWorkspace = workspace.workspace_id;
+    //                 console.log(`Using Watson Conversation with username ${conversationUsername}  and workspace ${conversationWorkspace}`);
+    //                 callback({
+    //                     error: false
+    //                 });
 
-                } else {
-                    console.log('Importing workspace from ./conversation/conversation.json');
-                    // Create the workspace
-                    const watsonWorkspace = JSON.parse(fs.readFileSync('./conversation/conversation.json'));
-                    // Force the name
-                    watsonWorkspace.name = 'Conversation';
-                    conversation.createWorkspace(watsonWorkspace, (createErr, workspace) => {
-                        if (createErr) {
-                            console.log(`Failed to create workspace ${err}`)
-                            callback({
-                                error: true,
-                                description: "Failed to create workspace"
-                            });
-                        } else {
-                            conversationWorkspace = workspace.workspace_id;
-                            console.log('Successfully created the workspace Conversation Demo');
-                            console.log(`Using Watson Conversation with username ${conversationUsername} and workspace ${conversationWorkspace}`);
-                            callback({
-                                error: false
-                            });
-                        }
-                    });
-                }
-            }
-        })
-    } else {
-        console.log('Workspace ID was specified as an environment variable.');
-        console.log(`Using Watson Conversation with username ${conversationUsername} and workspace ${conversationWorkspace}`);
-        callback({
-            error: false
-        });
-    }
+    //             } else {
+    //                 console.log('Importing workspace from ./conversation/conversation.json');
+    //                 // Create the workspace
+    //                 const watsonWorkspace = JSON.parse(fs.readFileSync('./conversation/conversation.json'));
+    //                 // Force the name
+    //                 watsonWorkspace.name = 'Conversation';
+    //                 conversation.createWorkspace(watsonWorkspace, (createErr, workspace) => {
+    //                     if (createErr) {
+    //                         console.log(`Failed to create workspace ${err}`)
+    //                         callback({
+    //                             error: true,
+    //                             description: "Failed to create workspace"
+    //                         });
+    //                     } else {
+    //                         conversationWorkspace = workspace.workspace_id;
+    //                         console.log('Successfully created the workspace Conversation Demo');
+    //                         console.log(`Using Watson Conversation with username ${conversationUsername} and workspace ${conversationWorkspace}`);
+    //                         callback({
+    //                             error: false
+    //                         });
+    //                     }
+    //                 });
+    //             }
+    //         }
+    //     })
+    // } else {
+    //     console.log('Workspace ID was specified as an environment variable.');
+    //     console.log(`Using Watson Conversation with username ${conversationUsername} and workspace ${conversationWorkspace}`);
+    //     callback({
+    //         error: false
+    //     });
+    // }
 }
 
 
@@ -142,14 +150,19 @@ module.exports = {
     sendMessage: (req, callback) => {
         let params = req.body.params;
         params.workspace_id = conversationWorkspace;
-        conversation.message(params, function (err, data) {
-            if (err) {
-                console.log(`Error in sending message:  ${err}`);
-                return callback(err);
-            } else {
-                const conv = data.context.conversation_id;
-                return callback(null, data);
-            }
-        })
+        request.post('http://localhost:3000/api/bot/conversation', )
+        // conversation.message(params, function (err, data) {
+        //     if (err) {
+        //         console.log(`Error in sending message:  ${err}`);
+        //         return callback(err);
+        //     } else {
+        //         const conv = data.context.conversation_id;
+        //         return callback(null, data);
+        //     }
+        // })
     }
 }
+
+
+
+
